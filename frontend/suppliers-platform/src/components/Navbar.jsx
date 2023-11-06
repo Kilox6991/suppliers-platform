5939d531e972dc2823e1ca1886b0441bb65e8170
Kilox6991 <4inf.mpromero@gmail.com>
2023-11-06 12:14:23 +0100

    Navbar component

--- frontend/suppliers-platform/src/components/Navbar.jsx
+++ frontend/suppliers-platform/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
+import Box from '@mui/material/Box';
 
 function Navbar() {
-    return(
-        <div>Navbar
-            <img src={logo} alt="logo-ct" />
-            <h2>CT Suppliers Platform v1.02</h2>
-        </div>
-    )
+    return (
+        <Box sx={{ display: "flex", backgroundColor: "#212529", gap:"20px" }}>
+            <img
+                src={logo}
+                alt="logo-ct"
+                style={{width:"200px", height:"100px"}}
+            />
+            <h2 style={{ color: "#c7c8c9", marginTop:"50px" }}>
+                CT Suppliers Platform v1.02
+            </h2>
+        </Box>
+    );
 }
 
-export default Navbar
+export default Navbar;
